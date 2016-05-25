f43a323c985ea192d09af44afc99a5bd8fabff6a
HBPro360 <jphamm360@gmail.com>
2016-05-24 19:01:13 -0500

    WorkingWithArrays

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] nums = new int[10];
+
+            for (int i=0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(0, 1000);
+                Console.WriteLine(i.ToString() + ". " + nums[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
